27df0be2deaaefdaae15b40c860c846ef97bec53
Evan Teng <mingqin10@gmail.com>
2017-03-05 22:35:01 -0500

    Movement for all directions as well as associated keys

--- jsAlphaScratched/js/player.js
+++ jsAlphaScratched/js/player.js
@@ -22,12 +22,34 @@ var playerclass = function (cr, map, stage, characterSheet) {
   }
 
   // Movement
+  this.changePosition = function(pos) {
+    this.position = pos;
+    this.character.gotoAndStop(pos);
+  }
   this.moveRight = function() {
     if (this.map.tileOccupied([this.col+1, this.row]) == false && this.map.tileWalkable([this.col+1, this.row])) {
       this.setCR([this.col+1, this.row]);
     }
+    this.changePosition("fcRight");
   }
-
+  this.moveLeft = function() {
+    if (this.map.tileOccupied([this.col-1, this.row]) == false && this.map.tileWalkable([this.col-1, this.row])) {
+      this.setCR([this.col-1, this.row]);
+    }
+    this.changePosition("fcLeft");
+  }
+  this.moveUp = function() {
+    if (this.map.tileOccupied([this.col, this.row-1]) == false && this.map.tileWalkable([this.col, this.row-1])) {
+      this.setCR([this.col, this.row-1]);
+    }
+    this.changePosition("fcBackward");
+  }
+  this.moveDown = function() {
+    if (this.map.tileOccupied([this.col, this.row+1]) == false && this.map.tileWalkable([this.col, this.row+1])) {
+      this.setCR([this.col, this.row+1]);
+    }
+    this.changePosition("fcForward");
+  }
 
   this.setXY();
 
