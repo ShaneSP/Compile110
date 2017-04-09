2d5335050f6fdbb705d474d694bcba24e13f5b1e
guanyuez <guanyuez@live.unc.edu>
2017-04-09 17:52:38 -0400

    added sample textbox text

---
+++ jsAlphaScratched/js/showText.js
@@ -0,0 +1,10 @@
+var showText = function(target, message, index, interval) {
+  if (index < message.length) {
+    $(target).append(message[index++]);
+    setTimeout(function() {showText(target, message, index, interval);}, interval);
+  }
+}
+
+$(function () {
+  showText("#msg", "This is a test message. I want to see how it will show up on the webpage.", 0, 100);
+});
