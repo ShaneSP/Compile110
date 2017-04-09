bce8df2f522b2b771ad7242fb5f5d5d0e06af676
guanyuez <guanyuez@live.unc.edu>
2017-04-09 19:01:27 -0400

    border around text box

--- jsAlphaScratched/js/showText.js
+++ jsAlphaScratched/js/showText.js
@@ -6,5 +6,5 @@ var showText = function(target, message, index, interval) {
 }
 
 $(function () {
-  showText("#msg", "This is a test message. I want to see how it will show up on the webpage.", 0, 100);
+  showText("#textbox", "This is a test message. I want to see how it will show up on the webpage.", 0, 100);
 });
