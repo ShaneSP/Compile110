a9f1cc502c25903ff3e0f0eb9fc2f7b447824464
Evan Teng <mingqin10@gmail.com>
2017-04-10 20:12:34 -0400

    Unecessarily smooth energy bar animation

--- jsAlphaScratched/js/menu/energybar.js
+++ jsAlphaScratched/js/menu/energybar.js
@@ -5,9 +5,29 @@ var energybar = function(bars, topstage) {
 
   this.barsprite = new createjs.SpriteSheet({
     "images": ["assets/energy2.png"],
-    "frames": {"height": 27, "width": 12, "count": 4},
+    "frames": {"height": 27, "width": 12, "count": 23},
     "animations": {
-      "fullEnd": [0], "emptyEnd": [1], "full": [2], "empty": [3]
+      "fullEnd": [0], "emptyEnd": [1], "full": [2], "empty": [3],
+      "drainingEnd": {
+        "frames": [15, 16, 17, 18, 19, 20, 21, 22],
+        "speed": 0.5,
+        "next": "emptyEnd"
+      },
+      "draining": {
+        "frames": [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+        "speed": 0.5,
+        "next": "empty"
+      },
+      "increasingEnd": {
+        "frames": [22, 21, 20, 19, 18, 17, 16, 15],
+        "speed": 0.5,
+        "next": "fullEnd"
+      },
+      "increasing": {
+        "frames": [14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4],
+        "speed": 0.5,
+        "next": "full"
+      }
     }
   });
 
@@ -33,22 +53,22 @@ var energybar = function(bars, topstage) {
 
   this.removeBar = function() {
     if (this.barnum == this.fullbars) {
-      this.bar[this.barnum-1].gotoAndPlay("emptyEnd");
+      this.bar[this.barnum-1].gotoAndPlay("drainingEnd");
       this.fullbars = this.fullbars - 1;
     }
     else if (this.fullbars != 0) {
-      this.bar[this.fullbars-1].gotoAndPlay("empty");
+      this.bar[this.fullbars-1].gotoAndPlay("draining");
       this.fullbars = this.fullbars - 1;
     }
   }
 
   this.addBar = function() {
     if(this.barnum == this.fullbars + 1) {
-      this.bar[this.fullbars].gotoAndPlay("fullEnd");
+      this.bar[this.fullbars].gotoAndPlay("increasingEnd");
       this.fullbars = this.fullbars + 1;
     }
     else if(this.barnum > this.fullbars) {
-      this.bar[this.fullbars].gotoAndPlay("full");
+      this.bar[this.fullbars].gotoAndPlay("increasing");
       this.fullbars = this.fullbars + 1;
     }
   }
