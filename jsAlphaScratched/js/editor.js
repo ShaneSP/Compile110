31d6260e2191373f4b3c8d75950c8a8bb4d1bc4f
guanyuez <eugenezhang@dhcp-v880-13-00813.1Xwireless.unc.edu>
2017-04-17 16:09:55 -0400

    cleaned up html page in indexScratched
    put editor script into a separate js file and moved our css to a
    separate css file

---
+++ jsAlphaScratched/js/editor.js
@@ -0,0 +1,7 @@
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/monokai");
+editor.getSession().setMode("ace/mode/javascript");
+var enemycode = ace.edit("editor2");
+enemycode.setReadOnly(true);
+enemycode.setTheme("ace/theme/monokai");
+enemycode.getSession().setMode("ace/mode/javascript");
