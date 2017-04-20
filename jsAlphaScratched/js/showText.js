884cad17288192cb511b9408008b98d44a4da0eb
guanyuez <eugenezhang@dhcp-v880-9-00133.1Xwireless.unc.edu>
2017-04-20 14:31:01 -0400

    added level maps and text

--- jsAlphaScratched/js/showText.js
+++ jsAlphaScratched/js/showText.js
@@ -5,6 +5,14 @@ var showText = function(target, message, index, interval) {
   }
 }
 
+// $(function () {
+//  showText("#textbox", "Welcome to compile(110);! Hope you enjoy your stay! ;)", 0, 50);
+// });
+
 $(function () {
-  showText("#textbox", "Welcome to compile(110);! Hope you enjoy your stay! ;)", 0, 50);
+  showText("#textbox", intro, 0, 50);
 });
+
+var intro = "Hello student! it seems you have been transported to a strange new world!";
+var intro2 = "In this world, coding is EVERYTHING!"
+var par1 = "This is the first paragraph.";
