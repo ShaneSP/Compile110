64c77325e070a91f36c1ccf372b65c9073dc920e
guanyuez <eugenezhang@dhcp-v880-9-00133.1Xwireless.unc.edu>
2017-04-20 15:07:10 -0400

    created button to advance the text in textbook

--- jsAlphaScratched/js/showText.js
+++ jsAlphaScratched/js/showText.js
@@ -1,3 +1,7 @@
+var intro = "Hello student! It seems you have been transported to a strange new world!";
+var intro2 = "In this world, coding is EVERYTHING!"
+var par1 = "This is the first paragraph.";
+
 var showText = function(target, message, index, interval) {
   if (index < message.length) {
     $(target).append(message[index++]);
@@ -13,6 +17,10 @@ $(function () {
   showText("#textbox", intro, 0, 50);
 });
 
-var intro = "Hello student! it seems you have been transported to a strange new world!";
-var intro2 = "In this world, coding is EVERYTHING!"
-var par1 = "This is the first paragraph.";
+var nextText = (function() {
+  var textArray = [intro, intro2, par1];
+  var count = -1;
+  return function() {
+    return textArray[++count % textArray.length];
+  }
+}());
