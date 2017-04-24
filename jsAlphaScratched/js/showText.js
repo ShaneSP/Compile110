d3bbf34c1beec393248786fd92f7b4a394d24e15
Evan Teng <mingqin10@gmail.com>
2017-04-23 23:35:01 -0400

    Nicer UI elements, minor text fixes, talksprite.

--- jsAlphaScratched/js/showText.js
+++ jsAlphaScratched/js/showText.js
@@ -1,24 +1,29 @@
-var linesoftext = ["Hello student! It seems you have been transported to a strange new world!",
-                    "In this world, coding is EVERYTHING!",
-                    "You will need to code to defend yourself against enemies.",
-                    "Like this one!",
-                    "FIN."];
+var linesoftext = [["Hello student! It seems you", "have been transported to a", "strange new world!"],
+                    ["In this world, coding is", "EVERYTHING!"],
+                    ["You will need to code to", "defend yourself against", "enemies."],
+                    ["Like this one!"],
+                    ["FIN."]];
 var currentline = 0;
 var globalinterval = 40;
 
-var showText = function(target, message, index, interval) {
-  if (index < message.length && linesoftext[currentline] == message) {
-    $(target).append(message[index++]);
-    setTimeout(function() {showText(target, message, index, interval);}, interval);
+var showText = function(target, message, line, index, interval) {
+  if (line < message.length && index < message[line].length && linesoftext[currentline] == message) {
+    $(target).append(message[line][index++]);
+    setTimeout(function() {showText(target, message, line, index, interval);}, interval);
+  }
+  else if (line < message.length && linesoftext[currentline] == message) {
+    line = line + 1;
+    $(target).append("<br/>");
+    setTimeout(function() {showText(target, message, line, 0, interval);}, interval);
   }
 }
 
 $(function () {
-  showText("#textbox", linesoftext[0], 0, globalinterval);
+  showText("#text_target", linesoftext[0], 0, 0, globalinterval);
 });
 
 var nextText = (function(target) {
   currentline = (currentline + 1) % linesoftext.length;
   $(target).empty();
-  showText(target, linesoftext[currentline], 0, globalinterval);
+  showText(target, linesoftext[currentline], 0, 0, globalinterval);
 });
