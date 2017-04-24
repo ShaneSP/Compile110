fa7384e893c593fd8603dc4fccc699a34e44c7f4
guanyuez <guanyuez@live.unc.edu>
2017-04-24 11:46:51 -0400

    working on enemy code on click

---
+++ jsAlphaScratched/js/getEnemyCode.js
@@ -0,0 +1,5 @@
+document.getElementById("gameboard").addEventListener("click", getEnemyCode);
+
+function getEnemyCode() {
+  document.getElementById("editor2").innerHTML = "This is supposed to be enemy code.";
+}
