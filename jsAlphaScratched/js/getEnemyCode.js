5fa99638b4bba34c2da592963310e3105cd08340
guanyuez <eugenezhang@dhcp-v880-13-00509.1Xwireless.unc.edu>
2017-04-25 12:31:31 -0400

    nemey code -> enemy code

--- jsAlphaScratched/js/getEnemyCode.js
+++ jsAlphaScratched/js/getEnemyCode.js
@@ -1,5 +1,5 @@
 document.getElementById("gameboard").addEventListener("click", getEnemyCode);
 
 function getEnemyCode() {
-  enemycode.session.setValue("//This is what the nemey code will be.\n//There should be a break.");
+  enemycode.session.setValue("//This is where the enemy code will be.\n//There should be a break.");
 }
