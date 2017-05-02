3f81f9089341f536a4b2d5dac8e0c1c23c9c5da9
guanyuez <guanyuez@live.unc.edu>
2017-05-02 14:42:07 -0400

    mute embedded into Kris's head

--- jsAlphaScratched/js/setHalfVolume.js
+++ jsAlphaScratched/js/setHalfVolume.js
@@ -3,6 +3,18 @@ function setVolume() {
   myAudio.volume = 0.07;
 }
 
+var unmute = document.getElementById('facecontainer');
+
+unmute.onclick = function() {
+  var myAudio = document.getElementById("bgm");
+  if (myAudio.volume > 0.0) {
+    myAudio.volume = 0.0;
+  }
+  else {
+    myAudio.volume = 0.1;
+  }
+}
+
 var bump = new Audio('assets/bump.wav');
 bump.volume = .3;
 
