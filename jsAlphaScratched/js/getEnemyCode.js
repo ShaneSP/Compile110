07e07caca6e3bfdd849f8c9a8626c32e2a3fb607
guanyuez <guanyuez@live.unc.edu>
2017-05-02 14:12:10 -0400

    changed enemy code - added functions and variables

--- jsAlphaScratched/js/getEnemyCode.js
+++ jsAlphaScratched/js/getEnemyCode.js
@@ -1,9 +1,9 @@
 //document.getElementById("gameboard").addEventListener("click", getEnemyCode);
 
 function getEnemyCode() {
-  enemycode.session.setValue("if(bit.inRange(player)) {\n  bit.chargeEnergyBlast();\n}\nif(bit.inRange(player) && krisline > 28) {\n  bit.shootEnergyBlast();\n}");
+  enemycode.session.setValue("// Bit Functions and Variables:\n  bit.health = 1;\n  bit.chargeEnergyBlast();\n  bit.shootEnergyBlast();\n\n// Enemy Code:\nif(bit.inRange(player)) {\n  bit.chargeEnergyBlast();\n}\nif(bit.inRange(player) && krisline > 28) {\n  bit.shootEnergyBlast();\n}");
 }
 
 function getSwordCode() {
-  enemycode.session.setValue("// A tip from Kris: \n// You can pick the sword up using the \n// function player.pickUp(\"sword\"); \n// but you have to be next to it!");
+  enemycode.session.setValue("// You can pick the sword up using the \n// function player.pickUp(\"sword\");");
 }
