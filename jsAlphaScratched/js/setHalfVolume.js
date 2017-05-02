8333e1d9f332527037283c906d415cdcf7cf3eb2
Evan Teng <mingqin10@gmail.com>
2017-05-02 14:20:53 -0400

    Play bump sound!

--- jsAlphaScratched/js/setHalfVolume.js
+++ jsAlphaScratched/js/setHalfVolume.js
@@ -1,4 +1,11 @@
 function setVolume() {
-    var myAudio = document.getElementById("bgm");
-    myAudio.volume = 0.1;
+  var myAudio = document.getElementById("bgm");
+  myAudio.volume = 0.07;
+}
+
+var bump = new Audio('assets/bump.wav');
+bump.volume = .3;
+
+function playBump() {
+  bump.play();
 }
