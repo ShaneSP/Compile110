3d0a2314ba798bf2c2d6ffe2eb0ef13ee7311bc1
Evan Teng <mingqin10@gmail.com>
2017-05-01 20:35:19 -0400

    Fake enemy code edited to be a little more accurate

--- jsAlphaScratched/js/getEnemyCode.js
+++ jsAlphaScratched/js/getEnemyCode.js
@@ -1,5 +1,5 @@
 document.getElementById("gameboard").addEventListener("click", getEnemyCode);
 
 function getEnemyCode() {
-  enemycode.session.setValue("bit.chargeEnergyBlast();\nbit.shootEnergyBlast();");
+  enemycode.session.setValue("if(bit.inRange(player)) {\n  bit.chargeEnergyBlast();\n}\nif(bit.inRange(player) && krisline > 28) {\n  bit.shootEnergyBlast();\n}");
 }
