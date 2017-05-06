16f4a917105d139df92a31431a956bddf063044d
guanyuez <guanyuez@live.unc.edu>
2017-05-05 23:00:34 -0400

    fixed player code

--- jsAlphaScratched/js/gameelements/getEnemyCode.js
+++ jsAlphaScratched/js/gameelements/getEnemyCode.js
@@ -1,7 +1,7 @@
 //document.getElementById("gameboard").addEventListener("click", getEnemyCode);
 
 function getEnemyCode() {
-  enemycode.session.setValue("// Bit Functions and Variables:\n  bit.health = 1;\n  bit.chargeEnergyBlast();\n  bit.shootEnergyBlast();\n\n// Enemy Code:\nif(bit.inRange(player)) {\n  bit.chargeEnergyBlast();\n}\nif(bit.inRange(player) && krisline > 28) {\n  bit.shootEnergyBlast();\n}");
+  enemycode.session.setValue("// Bit Functions and Variables:\n\n  bit.health = 1;\n  bit.chargeEnergyBlast();\n  bit.shootEnergyBlast();\n\n// Enemy Code:\n\nif(bit.inRange(player)) {\n  bit.chargeEnergyBlast();\n}\nif(bit.inRange(player) && krisline > 28) {\n  bit.shootEnergyBlast();\n}");
 }
 
 function getSwordCode() {
@@ -13,5 +13,5 @@ function getPortalCode() {
 }
 
 function getPlayerCode() {
-  enemycode.session.setValue("// Player Functions:\n\n  player.moveUp();\n  // move up (accepts parameters)\n  player.moveDown();\n  // move down (accepts parameters)\n  player.moveLeft();\n  // move left (accepts parameters)\n  player.moveRight();\n  // move right (accepts parameters)\n\n  player.shieldUp();\n  // shield upwards\n  player.shieldDown();\n  // shield downwards\n  player.shieldLeft();\n  // shield towards the left\n  player.shieldRight();\n  // shield towards the right\n\n  player.pickUp()\n  // pick up an object\n  // (accepts parameters)\n\n  if player.hasSword = true;\n    player.attackUp();\n    // attack upwards\n    player.attackDown();\n    // attack downards\n    player.attackLeft();\n    // attack towards the left\n    player.attackRight();\n    // attack towards the right\n");
+  enemycode.session.setValue("// Player Functions:\n\n  player.moveUp();\n  // move up (accepts parameters)\n  player.moveDown();\n  // move down (accepts parameters)\n  player.moveLeft();\n  // move left (accepts parameters)\n  player.moveRight();\n  // move right (accepts parameters)\n\n  player.shieldUp();\n  // shield upwards\n  player.shieldDown();\n  // shield downwards\n  player.shieldLeft();\n  // shield towards the left\n  player.shieldRight();\n  // shield towards the right\n\n  player.pickUp()\n  // pick up an object\n  // (accepts parameters)\n\n  if (player.hasSword === true) {\n    player.attackUp();\n    // attack upwards\n    player.attackDown();\n    // attack downards\n    player.attackLeft();\n    // attack towards the left\n    player.attackRight();\n    // attack towards the right\n  }");
 }
