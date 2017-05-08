a24644da001e42ee2485a3809ff3368cf265cfe5
Evan Teng <mingqin10@gmail.com>
2017-05-08 15:25:39 -0400

    Dialogue

--- jsAlphaScratched/js/gameelements/dialogue.js
+++ jsAlphaScratched/js/gameelements/dialogue.js
@@ -18,17 +18,19 @@ var linesoftext = [["Hello student! It seems you", "have been transported to a",
                     ["This line of code is called", "a 'function'. All entities in", "this world can call functions."],
                     ["There are various functions", "you can call to control", "your character in this world."],
                     ["As we progress in this world,", "you will be learning more", "and more functions!"],
-                    ["You can move around with", "moveRight();, moveLeft();, ", "moveUp();, and moveDown();."],
-                    ["However! There are places", "you will be unable to go.", "Figure them out yourself."],
+                    ["Click on yourself to see", "what functions you can use!"],
                     ["Now, it's time... prepare to", "defeat the bit!"],
-                    ["From the enemy code we can", "see that the bit charges", "a blast, and then fires it."],
-                    ["To defeat the bit, we are", "going to call the", "shieldRight(); function."],
-                    ["Your shield should reflect", "the blast back at the bit and", "destroy it!"],
+                    ["There are multiple ways to", "kill the bit. See that sword", "up there?"],
+                    ["You can kill the bit with", "that sword; however, there is", "a secret way to kill it."],
+                    ["You have access to a shield", "already. I wonder what it", "does."],
+                    ["The bit can shoot blasts at", "you, right? Maybe that has to", "do with it."],
                     ["Use what you have learned", "to defeat the bit and advance", "to the next level!"],
+                    ["No matter what, you will need", "that sword to progress."],
+                    ["You exit the level by stepping", "on the portal with that", "sword, so get it."],
                     ["Starting now, once you click", "'continue', the enemy will", "start attacking! Good luck!"],
                     ["..."]
                   ];
-var importantlines = [2, 3, 13, 14, 26, 27];
+var importantlines = [2, 3, 13, 14, 29, 30];
 var currentline = 0;
 var globalinterval = 25;
 
@@ -41,7 +43,7 @@ var showText = function(target, message, line, index, interval) {
     RUNCODE=true;
     $("#runcode").prop("disabled", false);
     SPAWN=false;
-  } else if(currentline==28){
+  } else if(currentline==30){
     ATTACK=true;
     SPAWN=false;
   } else {
